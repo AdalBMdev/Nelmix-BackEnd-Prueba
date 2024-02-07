08461d85d9ee3b1f0f0a48fc0591da91455a1abf
Adalberto <adalbertobanks.dev@gmail.com>
2024-02-06 22:56:32 -0400

    Eliminacion en el script sql de los Store Procedure no usados

--- Nelmix/Nelmix/obj/Debug/net6.0/Nelmix.AssemblyInfo.cs
+++ Nelmix/Nelmix/obj/Debug/net6.0/Nelmix.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nelmix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64fd928e38482cc2d809afeb8410ad3cea49dc4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nelmix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nelmix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
